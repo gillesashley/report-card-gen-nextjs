059f22a744ecd6fcbae73891935809346e72b091
gillesashley <xylaray37@gmail.com>
2023-04-17 14:30:21 +0000

    modified report-buttton

--- src/pages/report/report-button.tsx
+++ src/pages/report/report-button.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
-
-function ReportButton() {
+export default function ReportButton() {
   const generateReport = () => {
     window.print();
   };
@@ -8,12 +6,13 @@ function ReportButton() {
   return (
     <>
       <div className="flex justify-end">
-        <button onClick={generateReport} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <button
+          onClick={generateReport}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
           Generate Report
         </button>
       </div>
     </>
   );
 }
-
-export default ReportButton;
